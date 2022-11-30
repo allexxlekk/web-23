32f09bba8dacf9efbd259609620d6d48919a0541
allexxlekk <allexxlekk@gmail.com>
2022-11-30 19:55:34 +0200

    Create login check functionality

--- login.js
+++ login.js
@@ -4,12 +4,26 @@ const userData = [
   { username: "kogi4ever", password: "pubgMono" },
 ];
 
-function check_login(username, password) {
-  userData.forEach((user) => {
-    if (username === user.username) {
-      if (password === user.password) return true;
-      return false;
+function login(username, password) {
+  for (let i = 0; i < userData.length; i++) {
+    if (userData[i].username === username) {
+      if (userData[i].password === password) return true;
     }
     return false;
-  });
+  }
+  return false;
 }
+
+const loginSelect = document.querySelector(".loginSelect");
+loginSelect.addEventListener("click", (e) => {
+  const loginForm = document.querySelector("#login-form");
+  loginForm.classList.toggle("hidden");
+});
+
+const loginButton = document.querySelector(".loginButton");
+loginButton.addEventListener("click", (e) => {
+  const inputs = document.querySelectorAll("input");
+  const username = inputs[0].value;
+  const password = inputs[1].value;
+  console.log(login(username, password));
+});
